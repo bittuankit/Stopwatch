2f399c03ddf3160bc6fe449bdcf17c3643829b8c
Ankit Gupta <iambittuankitbkg@gmail.com>
2025-01-24 22:04:42 +0530

    time lap added

--- script.js
+++ script.js
@@ -2,15 +2,16 @@ const time = document.querySelector(".time");
 const startBtn = document.getElementById("start");
 const resetBtn = document.getElementById("reset");
 const lapBtn = document.getElementById("lap");
+const lapContainer = document.querySelector(".lap-container");
 
 let timer = 0;
 let start = 0;
 let elapsed = 0;
 let isRunning = false;
+let hours = 0;
 
 startBtn.addEventListener("click", () => {
   startBtn.textContent === "Start" ? startTime() : stopTime();
-  console.log(startBtn.textContent);
 });
 
 const startTime = () => {
@@ -32,6 +33,7 @@ const stopTime = () => {
 };
 
 resetBtn.addEventListener("click", () => {
+  startBtn.textContent = "Start";
   clearInterval(timer);
   start = 0;
   elapsed = 0;
@@ -43,13 +45,27 @@ const updateTime = () => {
   const currentTime = Date.now();
   elapsed = currentTime - start;
 
-  let hours = String(Math.floor(elapsed / (1000 * 60 * 60))).padStart(2, "0");
-  let minutes = String(Math.floor((elapsed / (1000 * 60)) % 60)).padStart(
-    2,
-    "0"
-  );
-  let seconds = String(Math.floor((elapsed / 1000) % 60)).padStart(2, "0");
-  let millisecond = String(Math.floor((elapsed % 1000) / 10)).padStart(2, "0");
+  hours = String(Math.floor(elapsed / (1000 * 60 * 60))).padStart(2, "0");
+  minutes = String(Math.floor((elapsed / (1000 * 60)) % 60)).padStart(2, "0");
+  seconds = String(Math.floor((elapsed / 1000) % 60)).padStart(2, "0");
+  millisecond = String(Math.floor((elapsed % 1000) / 10)).padStart(2, "0");
 
   hours = time.textContent = `${hours}:${minutes}:${seconds}:${millisecond}`;
 };
+
+lapBtn.addEventListener("click", () => {
+  const div = document.createElement("div");
+  const innerDivHtml = `<div class="laps">
+            <div class="lap">${hours}</div>
+            <i class="fa-solid fa-trash"></i>
+        </div>`;
+  div.innerHTML = innerDivHtml;
+  lapContainer.append(div);
+});
+
+lapContainer.addEventListener("click", (e) => {
+  if (e.target.classList.contains("fa-trash")) {
+    const timeLap = e.target.parentNode;
+    timeLap.style.display = "none";
+  }
+});
